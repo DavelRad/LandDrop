30f102bc8422aa5262ed2146b07b763e0d660cb9
Ryan Pham <61821603+flotoria@users.noreply.github.com>
2024-11-09 18:58:17 -0800

    save everything to a json state file regarding soil data, location, summary, risk percentage, city name (#5)
    * save everything to a json state file
    
    * return json

--- backend/agent_funcs.py
+++ backend/agent_funcs.py
@@ -1,7 +1,8 @@
 from dotenv import load_dotenv
 import json
 from api.ai import talk_to_chatbot
-from instructions import RISK_SUMMARY_PROMPT, RISK_PERCENTAGE_PROMPT
+import re
+from instructions import RISK_SUMMARY_PROMPT, RISK_PERCENTAGE_PROMPT, DROUGHT_RISK_INSTRUCTIONS
 
 
 load_dotenv()
@@ -38,3 +39,23 @@ def risk_percentage(data, summary):
 ]
     response = talk_to_chatbot(messages)
     return response
+
+def drought_risk(data):
+    """
+    Function to return the drought risk based on the data.
+    """
+    messages = [
+        {
+            "role": "system",
+            "content": f"{DROUGHT_RISK_INSTRUCTIONS}"
+        },
+        {
+            "role": "user",
+            "content": f"{data}"
+        }
+    ]
+
+    response = talk_to_chatbot(messages)
+    cleaned_response = re.sub(r'^```json|```$', '', response.strip())
+    json_string = json.loads(cleaned_response)
+    return json_string
--- backend/api/location.py
+++ backend/api/location.py
@@ -1,5 +1,6 @@
 import requests
 import os
+from .ai import talk_to_chatbot
 
 GEOAPIFY_API_KEY = os.getenv("GEOAPIFY_API_KEY")
 
@@ -9,5 +10,34 @@ def get_address_from_coords(lat: float, lon: float):
         response.raise_for_status()  
         responseJSON = response.json()
         return f"{responseJSON["features"][0]["properties"]["city"]}, {responseJSON["features"][0]["properties"]["state"]}, {responseJSON["features"][0]["properties"]["country"]}"
+    except Exception as e:
+        raise SystemExit(f"Failed to make the request. Error: {e}")
+    
+def get_population_from_coords(lat: float, lon: float):
+    try:
+        address = get_address_from_coords(lat, lon)
+
+        messages = [
+            {
+                "role": "system",
+                "content": [
+                    {
+                        "type": "text",
+                        "text": "You are an AI assistant that helps the user find the population based off a city, state, and country. The user will input the city, state, and country. Only send back the population as a number and nothing else."
+                    }
+                ]
+            },
+            {
+                "role": "user",
+                "content": [
+                    {
+                        "type": "text",
+                        "text": f"{address}"
+                    }
+                ]
+            }
+        ]
+        
+        return talk_to_chatbot(messages)
     except Exception as e:
         raise SystemExit(f"Failed to make the request. Error: {e}")
